7c6ba39bdbaccf31dd86c9d1503937597c705be2
Species <hejizheng2004@163.com>
2018-12-02 14:13:15 +0800

    first code merge: player adding - Daniel commit

---
+++ TacticsMove.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticsMove : Init {
+    const int INF = 0x7fffffff; // Infinity value
+    
+    public struct Point { // stores a pair of coordinates of a tile
+        public int x, y;
+        public Point(int x, int y) { // used for assigning tiles
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    // array of moving; up - down - left - right
+    public int[] dirx = new int[4] { 0, 0, -1, 1 };
+    public int[] diry = new int[4] { -1, 1, 0, 0 };
+
+    public List<Tile> eyelist = new List<Tile>(); // List of the tiles that the player can see
+    public List<Tile> movelist = new List<Tile>(); // List of the tiles that the player can move to
+
+    public Point[,] prev = new Point[MapLen + 1, MapWid + 1]; // Record the path of every tile
+
+    Tile currentTile; // marking the tile that the player is standing
+    int currentx, currenty;
+
+    // Find the tiles that the players can see
+    public void SpfaEye(Tile t) {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        foreach (GameObject tile in tiles) { // Recover Fog
+            tile.GetComponent<Tile>().insight = false;
+        }
+
+        int[,] dis = new int[MapLen + 1, MapWid + 1];
+        bool[,] visited = new bool[MapLen + 1, MapWid + 1];
+        Queue<Point> q = new Queue<Point>();
+
+        for (int i = 1; i <= MapLen; i++) {
+            for (int j = 1; j <= MapWid; j++) {
+                dis[i, j] = INF;
+                visited[i, j] = false;
+            }
+        }
+        q.Clear();
+
+        dis[t.x, t.y] = 0;
+        Point push = new Point(t.x, t.y);
+        q.Enqueue(push);
+        visited[t.x, t.y] = true;
+
+        while (q.Count > 0) {
+            Point u = q.Dequeue();
+            visited[u.x, u.y] = false;
+
+            for (int i=0; i<4; i++) {
+                Point v = new Point(u.x + dirx[i], u.y + diry[i]);
+                if (Valid(v) && dis[u.x, u.y] + eyecost[MapType[v.x, v.y]] < dis[v.x, v.y]) {
+                    dis[v.x, v.y] = dis[u.x, u.y] + eyecost[MapType[v.x, v.y]];
+                    if (!visited[v.x, v.y] && dis[v.x, v.y] <= maxeye) {
+                        Point ppush = new Point(v.x, v.y);
+                        q.Enqueue(ppush);
+                        visited[v.x, v.y] = true;
+                    }
+                }
+            }
+        }
+
+        for (int i=1; i<=MapLen; i++) {
+            for (int j=1; j<=MapWid; j++) {
+                if (dis[i, j] <= maxeye) {
+                    GameObject row = GameObject.Find("Row" + i.ToString());
+                    GameObject tile = row.transform.Find("Tile" + j.ToString()).gameObject;
+                    eyelist.Add(tile.GetComponent<Tile>());
+                    tile.GetComponent<Tile>().insight = true;
+                    tile.GetComponent<Tile>().eyedis = dis[i, j];
+                }
+            }
+        }
+    }
+
+    public void SpfaMove(Tile t) {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        foreach (GameObject tile in tiles) {
+            tile.GetComponent<Tile>().selectable = false;
+            tile.GetComponent<Tile>().selected = false;
+        }
+
+        int[,] dis = new int[MapLen + 1, MapWid + 1];
+        bool[,] visited = new bool[MapLen + 1, MapWid + 1];
+        Queue<Point> q = new Queue<Point>();
+
+        for (int i = 1; i <= MapLen; i++) {
+            for (int j = 1; j <= MapWid; j++) {
+                dis[i, j] = INF;
+                visited[i, j] = false;
+            }
+        }
+        q.Clear();
+
+        dis[t.x, t.y] = 0;
+        Point push = new Point(t.x, t.y);
+        q.Enqueue(push);
+        visited[t.x, t.y] = true;
+
+        while (q.Count > 0) {
+            Point u = q.Dequeue();
+            visited[u.x, u.y] = false;
+
+            for (int i = 0; i < 4; i++) {
+                Point v = new Point(u.x + dirx[i], u.y + diry[i]);
+                prev[v.x, v.y] = new Point(u.x, u.y);
+                if (dis[u.x, u.y] + movecost[MapType[v.x, v.y]] < dis[v.x, v.y]) {
+                    dis[v.x, v.y] = dis[u.x, u.y] + movecost[MapType[v.x, v.y]];
+                    if (!visited[v.x, v.y] && dis[v.x, v.y] <= maxmove) {
+                        Point ppush = new Point(v.x, v.y);
+                        q.Enqueue(ppush);
+                        visited[v.x, v.y] = true;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= MapLen; i++) {
+            for (int j = 1; j <= MapWid; j++) {
+                if (dis[i, j] <= maxmove) {
+                    GameObject row = GameObject.Find("Row" + i.ToString());
+                    GameObject tile = row.transform.Find("Tile" + j.ToString()).gameObject;
+                    movelist.Add(tile.GetComponent<Tile>());
+                    tile.GetComponent<Tile>().selectable = true;
+                    tile.GetComponent<Tile>().movedis = dis[i, j];
+                }
+            }
+        }
+    }
+
+    public void GetCurrentTile() {
+        currentTile = GetTargetTile(gameObject);
+        currentTile.current = true;
+    }
+
+    public Tile GetTargetTile(GameObject target) {
+        RaycastHit hit;
+        Tile tile = null;
+
+        Physics.Raycast(target.transform.position, -Vector3.up, out hit, 1);
+        tile = hit.collider.GetComponentInParent<Tile>();
+
+        return tile;
+    }
+
+    public void FindPath() {
+        GetCurrentTile();
+        GameObject row = currentTile.transform.parent.gameObject;
+        currentx = int.Parse(row.name.Split('w')[1]);
+        currenty = int.Parse(currentTile.name.Split('e')[1]);
+        SpfaEye(currentTile);
+        SpfaMove(currentTile);
+    }
+
+    public bool Valid(Point p) {
+        return p.x >= 0 && p.x <= MapLen && p.y >= 0 && p.y <= MapWid;
+    }
+
+    public void MoveToTile(GameObject tile) {
+        GameObject row = tile.transform.parent.gameObject;
+        int x = int.Parse(row.name.Split('w')[1]);
+        int y = int.Parse(tile.name.Split('e')[1]);
+
+        int i = x, j = y, tmpi = 0, tmpj = 0;
+        while (!(i == currentx && j == currenty)) {
+            tmpi = i;
+            tmpj = j;
+            i = prev[tmpi, tmpj].x;
+            j = prev[tmpi, tmpj].y;
+
+        } 
+    }
+}
